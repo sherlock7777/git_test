e09d8f41ba0120f625ced692ea822e61f1e24c52
sherlock <798522378@qq.com>
2019-04-03 17:27:44 +0800

    修改test

--- src/com/itheima/test/GitTest.java
+++ src/com/itheima/test/GitTest.java
@@ -3,5 +3,6 @@ package com.itheima.test;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hahahaha");
+        System.out.println("hehehehe");
     }
 }
